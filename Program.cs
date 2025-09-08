8e00ec816ad08709147c585cf6f4aaf866e2b869
LeyLey30 <Wesley30br@gmail.com>
2025-09-08 08:28:02 -0300

    Cp
    Correção de código

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int altura = 10; // altura da árvore
+        // Desenha a parte triangular da árvore
+        for (int i = 1; i <= altura; i++)
+        {
+            // Espaços à esquerda
+            Console.Write(new string(' ', altura - i));
+            // Asteriscos da árvore
+            Console.WriteLine(new string('*', 2 * i - 1));
+        }
+        // Desenha o tronco da árvore
+        for (int i = 0; i < altura / 3; i++)
+        {
+            Console.Write(new string(' ', altura - 1));
+            Console.WriteLine("|");
+        }
+    }
+}
